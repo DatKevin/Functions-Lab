cfd641365a738910a3524544fe4e67eb77c40eba
Kevin Dat Nguyen <datkevin@gmail.com>
2019-10-22 13:02:20 -0700

    Fixed and finished Problem 4

--- Problem-4.js
+++ Problem-4.js
@@ -2,30 +2,20 @@ let pandigital = (number) => {
 	/// Converts a number into an array to compare each individual digit
 	let numberString = String(number)
 	let split = numberString.split("")
-	/// Compares each number by index. If it's a new number, store it in an array called numberBox.
-	/// If there is a copy of that number in the existing numberBox array, instantly return false
+	/// Number is pandigital till proven otherwise
 	let isfalse = true
-	let numberBox = []
-	/*for (let i=0; i<split.length; i++) {
-		/// removes the number from the array and then attempts to find the number within the remaining array
-		let lookingFor = split[i]
-		split.shift()
-		numberChecker = split.prototype.find(lookingFor)
-		if (numberChecker == true) {
-			return isfalse = false
-		}
-	}
-	*/
-	for (let i=0; i<split.length; i++) {
-		if (split[i] in numberBox) {
+	/// Sorts all number in order
+	let sortedNumber = split.slice().sort()
+	/// Compares sorted numbers with an actual 1-n list. If there are differences
+	/// isfalse is set to false to show it is not pandigital
+		for (let i=1; i<split.length; i++) {
+		if (sortedNumber[i-1] != String(i)) {
 			isfalse = false
-			console.log(false)
-
 		} 
-		else {
-			numberBox[i] = split[i]
-			console.log(numberBox)
-		} 
+	}
+	/// Checks for any 0s as any number with 0 is not 1 to n pandigital
+	if (sortedNumber[0] == "0") {
+		isfalse = false
 	}
 	if (isfalse == false) {
 		console.log("The number is not pandigital")
@@ -35,5 +25,8 @@ let pandigital = (number) => {
 	}
 
 }
-pandigital(248709)
-pandigital(999)
+pandigital(231)
+pandigital(942)
+pandigital(0)
+pandigital(987654321)
+pandigital(120)
